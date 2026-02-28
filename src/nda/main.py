a1704fbccd3d645b4fe8c53715183b817ae8e3b4
Gabriel Fuentes <gafnts@gmail.com>
2026-02-27 19:58:54 -0600

    Update NDA schema

--- src/nda/main.py
+++ src/nda/main.py
@@ -2,19 +2,25 @@ from pathlib import Path
 from nda.data_loader import DataLoader, Partition
 
 
-def main() -> None:
+DATA_DIR: Path = Path(__file__).parent / "static" / "data"
+OUTPUT_DIR: Path = Path(__file__).parent / "static" / "outputs"
+PARTITIONS: tuple[Partition, Partition, Partition] = ("train", "dev-0", "test-A")
+
+
+def load_data() -> None:
     loader = DataLoader(
-        data_dir=Path(__file__).parent / "static" / "data",
-        output_dir=Path(__file__).parent / "static" / "outputs",
+        data_dir=DATA_DIR,
+        output_dir=OUTPUT_DIR,
     )
 
-    partitions: tuple[Partition, Partition, Partition] = ("train", "dev-0", "test-A")
-    train, val, test = [loader.load(partition) for partition in partitions]
+    train, val, test = [loader.load(partition) for partition in PARTITIONS]
 
-    [
+    for df, partition in zip((train, val, test), PARTITIONS):
         loader.export(df, partition)
-        for df, partition in zip((train, val, test), partitions)
-    ]
+
+
+def main() -> None:
+    load_data()
 
 
 if __name__ == "__main__":
